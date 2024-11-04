6a4cdada14cfd590bcc22ff3b9d613c2ff452005
Tubos <loureiromanuel@itis-molinari.eu>
2024-11-04 14:00:47 +0100

    funzioni

---
+++ Scripts/functions.js
@@ -0,0 +1,13 @@
+const hide = (div) => {
+    let hidden = '<class="hide">';
+    hidden += div;
+    hidden += '</class>'
+    innerHTML = hidden; 
+}
+
+const show = (div) => {
+    let hidden = '<class="show">';
+    hidden += div;
+    hidden += '</class>'
+}
+
